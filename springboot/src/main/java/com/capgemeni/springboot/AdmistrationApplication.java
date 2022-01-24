cced22e434e58e07494bcdedaef6becf875ad662
prasannamanchuri <59312022+prasannamanchuri@users.noreply.github.com>
2022-01-24 20:35:58 +0530

    Add files via upload

---
+++ springboot/src/main/java/com/capgemeni/springboot/AdmistrationApplication.java
@@ -0,0 +1,12 @@
+package com.capgemeni.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmistrationApplication {
+    
+	public static void main(String[] args) {
+		SpringApplication.run( AdmistrationApplication.class, args);
+	}
+}
