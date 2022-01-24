414ee3615386da35cfa7ba0ee78773a76f3699aa
prasannamanchuri <59312022+prasannamanchuri@users.noreply.github.com>
2022-01-24 19:09:23 +0530

    Delete springboot directory

--- springboot/src/main/java/com/capgemeni/springboot/service/RailwayAdminstrationService.java
+++
@@ -1,8 +0,0 @@
-package com.capgemeni.springboot.service;
-
-public interface RailwayAdminstrationService {
-	public double login(String Username,String Password);
-	public double register(String Username,String Password);
-	public double getuserlist(String userId);
-	
-}
